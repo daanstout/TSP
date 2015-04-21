3934ac7a4ac8208805fc3374f854893bcf57e04c
maestroke <formaestroke@gmail.com>
2015-04-21 14:08:23 +0200

    order

--- src/tsp/Order.java
+++ src/tsp/Order.java
@@ -5,10 +5,25 @@
  */
 package tsp;
 
+import java.util.ArrayList;
+
 /**
  *
  * @author Coolbone
  */
 public class Order {
     
+    private int orderNummer;
+    private ArrayList<Artikel> productLijst;
+    private Klant klant;
+    
+    public Order(int orderNummer, Klant klant){
+        this.orderNummer = orderNummer;
+        this.klant = klant;
+        productLijst = new ArrayList<>();
+    }
+    
+    public void addProduct(Artikel a){
+        productLijst.add(a);
+    }
 }
