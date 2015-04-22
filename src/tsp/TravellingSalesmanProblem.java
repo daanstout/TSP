eea3aaed174bff9d7c76f91396f06d743c1fe813
maestroke <formaestroke@gmail.com>
2015-04-22 11:29:32 +0200

    edits

--- src/tsp/TravellingSalesmanProblem.java
+++ src/tsp/TravellingSalesmanProblem.java
@@ -4,19 +4,24 @@ package tsp;
 import java.util.ArrayList;
 
 
-public class TravellingSalesmanProblem {
+    public class TravellingSalesmanProblem {
+    
     private Magazijn magazijn;
     private ArrayList<Artikel> routeData;
     private String Resultaat;
+        
+    
+    public TravellingSalesmanProblem(Magazijn magazijn){
+        this.magazijn = magazijn;
+        routeData = new ArrayList<>();
+    }
 
+    public String setResultaat() {
+        return Resultaat;
+    }
 
-public TravellingSalesmanProblem(Magazijn magazijn){
-    this.magazijn = magazijn;
-    routeData = new ArrayList<>();
-}
-
-public String setResultaat() {
-    return Resultaat;
-}
+    public void addArtikel(Artikel a){
+        routeData.add(a);
+    }
 
 }
